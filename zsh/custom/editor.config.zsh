188cd56baaa518d977e4da38eb6dc13e4bef93a0
Ian Battersby <ian@Pathogen.local>
2018-09-04 20:15:03 +0100

    Initial commit.

---
+++ zsh/custom/editor.config.zsh
@@ -0,0 +1,8 @@
+export EDITOR='nvim'
+export VISUAL=$EDITOR
+
+alias vim='nvim'
+alias vi='nvim'
+alias oldvim='/usr/bin/vim'
+alias oldvi='/usr/bin/vi'
+alias vimdiff='nvim -d'
---
+++ zsh/custom/exa.config.zsh
@@ -0,0 +1,4 @@
+alias 'l'='exa'
+alias 'ls'='exa'
+alias 'll'='exa -l'
+alias 'lll'='exa -la'
