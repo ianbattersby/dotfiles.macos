0c2114c2af611888be2abedaf3c43a7b28630212
Ian Battersby <ian.battersby@gmail.com>
2018-09-10 16:17:47 +0100

    zsh: Improve custom configuration.

---
+++ zsh/custom/comp.config.zsh
@@ -0,0 +1,5 @@
+setopt complete_aliases
+setopt PROMPT_SUBST
+setopt CORRECT
+setopt COMPLETE_IN_WORD
+setopt IGNORE_EOF
---
+++ zsh/custom/keys.config.zsh
@@ -0,0 +1,10 @@
+bindkey -v
+bindkey '^[^[[D' backward-word
+bindkey '^[^[[C' forward-word
+bindkey '^[[5D' beginning-of-line
+bindkey '^[[5C' end-of-line
+bindkey '^[[A' up-line-or-beginning-search
+bindkey '^[[B' down-line-or-beginning-search
+bindkey '^\' history-incremental-search-backward
+bindkey '^[0a' history-incremental-pattern-search-backward
+bindkey '^[0b' history-incremental-pattern-search-forward
