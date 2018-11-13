981badc2aabd250f3809a7d0930264a25cf0a124
Ian Battersby <ian.battersby@gmail.com>
2018-11-13 18:46:39 +0000

    zsh: Use expansion paths for username agnostic installation.

--- zsh/custom/weechat.config.zsh
+++ zsh/custom/weechat.config.zsh
@@ -1,4 +1,4 @@
-alias weechat='LD_PRELOAD=/Users/ian/.weechat/libwcwidth.so /usr/local/bin/weechat'
+alias weechat='LD_PRELOAD=~/.weechat/libwcwidth.so /usr/local/bin/weechat'
 
 #PERL_MM_OPT="INSTALL_BASE=$HOME/perl5" cpan local::lib
 #echo 'eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib=$HOME/perl5)"' 
