278bb6f111629350b0dc8d17b819e416518e5b29
Ian Battersby <ian.battersby@gmail.com>
2019-01-02 17:33:49 +0000

    zsh: Make 'exit' tmux aware (thanks @fatih).

--- zsh/custom/tmux.config.zsh
+++ zsh/custom/tmux.config.zsh
@@ -1,1 +1,15 @@
+exit() {
+    if [[ -z $TMUX ]]; then
+        builtin exit
+        return
+    fi
+
+    nr=$(tmux list-panes | wc -l)
+    if [ $count = eq 1 ]; then
+        tmux detach
+    else
+        builtin exit
+    fi
+}
+
 alias tm='tmux attach -t base || (tmux -2 new-session -d -s `basename $USER`; tmux rename-window -t `basename $USER`:1 terminal; tmux attach -t `basename $USER`)'
