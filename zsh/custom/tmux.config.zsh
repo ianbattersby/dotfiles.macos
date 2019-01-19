bf6100ef73327ca5beee783d0915dce4fd208a54
Ian Battersby <ian.battersby@gmail.com>
2019-01-19 12:19:19 +0000

    zsh: Add ZSH_TMUX_TERM env variable for FreeBSD/Mosh.

--- zsh/custom/tmux.config.zsh
+++ zsh/custom/tmux.config.zsh
@@ -13,3 +13,5 @@ exit() {
 }
 
 alias tm='tmux attach -t base || (tmux -2 new-session -d -s `basename $USER`; tmux rename-window -t `basename $USER`:1 terminal; tmux attach -t `basename $USER`)'
+
+export ZSH_TMUX_TERM=xterm-256color
