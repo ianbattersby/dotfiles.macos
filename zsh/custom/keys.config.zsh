2c076f826ec3634b8d36d85910091a0fc25ce93f
Ian Battersby <ian.x.battersby@gsk.com>
2019-01-30 19:28:39 +0000

    zsh: Change incremental search key-bindings;
      * Invoke incremental-pattern-search with CTRL-S.
      * Use ^R (backward) and ^T (forward) to navigate incremental search.

--- zsh/custom/keys.config.zsh
+++ zsh/custom/keys.config.zsh
@@ -1,12 +1,12 @@
 bindkey -v
+
 bindkey "^[^[[D" backward-word
 bindkey "^[^[[C" forward-word
 bindkey "^[[5D" beginning-of-line
 bindkey "^[[5C" end-of-line
 bindkey "^[[A" up-line-or-beginning-search
 bindkey "^[[B" down-line-or-beginning-search
-bindkey "^\\" history-incremental-pattern-search-backward
-bindkey "^#" history-incremental-pattern-search-backward
+
+bindkey "^S" history-incremental-pattern-search-backward
 bindkey "^R" history-incremental-search-backward
-bindkey "^[" history-incremental-search-backward
-bindkey "^]" history-incremental-search-forward
+bindkey "^T" history-incremental-search-forward
