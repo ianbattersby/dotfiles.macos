479d54f40442f775cb22f8e4730812f3026669f9
Ian Battersby <ian.battersby@gmail.com>
2019-01-30 11:48:14 +0000

    zsh: Fix keybindings for incremental search by;
      * Introduce ^# for explicitness given OS X input source mapping.
      * Use ^[ (backward) and ^] (forward) for incremental search forward/backward.

--- zsh/custom/keys.config.zsh
+++ zsh/custom/keys.config.zsh
@@ -1,10 +1,12 @@
 bindkey -v
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[A' up-line-or-beginning-search
-bindkey '^[[B' down-line-or-beginning-search
-bindkey '^\' history-incremental-search-backward
-bindkey '^[0a' history-incremental-pattern-search-backward
-bindkey '^[0b' history-incremental-pattern-search-forward
+bindkey "^[^[[D" backward-word
+bindkey "^[^[[C" forward-word
+bindkey "^[[5D" beginning-of-line
+bindkey "^[[5C" end-of-line
+bindkey "^[[A" up-line-or-beginning-search
+bindkey "^[[B" down-line-or-beginning-search
+bindkey "^\\" history-incremental-pattern-search-backward
+bindkey "^#" history-incremental-pattern-search-backward
+bindkey "^R" history-incremental-search-backward
+bindkey "^[" history-incremental-search-backward
+bindkey "^]" history-incremental-search-forward
