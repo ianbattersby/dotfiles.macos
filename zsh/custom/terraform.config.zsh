3d93ef2dba2a0013a445b1338ff8a117bf2586c4
Ian Battersby <ian.battersby@gmail.com>
2020-05-23 14:14:49 +0100

    zsh: function to 'terraform fmt --recursive' by default.

--- zsh/custom/terraform.config.zsh
+++ zsh/custom/terraform.config.zsh
@@ -1,2 +1,11 @@
-alias tf='terraform'
-alias tfv='terraform fmt; terraform validate'
+#!/bin/bash
+
+function terraform_default_to_recursive() {
+  case $* in
+    fmt* ) shift 1; command terraform fmt --recursive "$@" ;;
+    * ) command terraform "$@" ;;
+  esac
+}
+
+alias tf='terraform_default_to_recursive'
+alias tfv='terraform fmt --recursive; terraform validate'
