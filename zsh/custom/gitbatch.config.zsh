112c4443ccea4d7c0cee5963e736e49ba53c3d85
Ian Battersby <ian.battersby@gmail.com>
2020-05-29 11:11:30 +0100

    zsh: Add alias for 5 most recent branches (gbrcnt).

--- zsh/custom/gitbatch.config.zsh
+++ zsh/custom/gitbatch.config.zsh
@@ -1,1 +1,10 @@
+function most_recent_branches() {
+    git for-each-ref \
+        --format="%(refname:short)" \
+        --count=5 \
+        --sort=-committerdate \
+        refs/heads/
+}
+
+alias gbrcnt='most_recent_branches'
 alias gbat='gitbatch -d ~/code -r 1'
