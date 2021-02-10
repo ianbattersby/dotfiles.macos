d65df683091a0a87f83c4ad9cd6b60f6dbc38d67
Ian Battersby <ian.battersby@gmail.com>
2021-02-10 14:02:25 +0000

    zsh: Add ZSH alises for arm + intel installs.

--- zsh/config/homebrew.config.zsh
+++ zsh/config/homebrew.config.zsh
@@ -1,1 +1,7 @@
 export HOMEBREW_NO_ANALYTICS=1
+
+alias ibrew='arch -x86_64 /usr/local/Homebrew/bin/brew'
+alias abrew='arch -arm64 /opt/Homebrew/bin/brew'
+alias brew='abrew'
+
+export PATH="/opt/Homebrew/bin:$PATH"
