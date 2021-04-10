701cd6d37799bcac782730eabad91c7c29eacd58
Ian Battersby <ian.battersby@gmail.com>
2021-04-10 16:33:03 +0100

    zsh: Smarter brew architecture detection.

--- zsh/config/homebrew.config.zsh
+++ zsh/config/homebrew.config.zsh
@@ -1,7 +1,15 @@
 export HOMEBREW_NO_ANALYTICS=1
 
+function brew_detect() {
+    if [[ "$(uname -m)" == "x86_64" ]]; then
+        command arch -x86_64 /usr/local/Homebrew/bin/brew "$@"
+    else
+        command arch -arm64 /opt/Homebrew/bin/brew "$@"
+    fi
+}
+
 alias ibrew='arch -x86_64 /usr/local/Homebrew/bin/brew'
 alias abrew='arch -arm64 /opt/Homebrew/bin/brew'
-alias brew='abrew'
+alias brew='brew_detect'
 
 export PATH="/opt/Homebrew/bin:$PATH"
