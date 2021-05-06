717f9411593cd9358feede5a5daf70c718935797
Ian Battersby <ian.battersby@gmail.com>
2021-05-06 22:46:27 +0100

    nvim: Add 'regex' to nvim-treesitter.

--- nvim/lua/plugins/lsp/treesitter.lua
+++ nvim/lua/plugins/lsp/treesitter.lua
@@ -1,6 +1,6 @@
 local function config()
   require'nvim-treesitter.configs'.setup {
-    ensure_installed = { "bash", "c_sharp", "css", "go", "gomod", "javascript", "json", "jsdoc", "lua", "python", "rust", "scss", "toml", "yaml" }, --"maintained", --vim.tbl_keys(require'languages')),
+    ensure_installed = { "bash", "c_sharp", "css", "go", "gomod", "javascript", "json", "jsdoc", "lua", "python", "regex", "rust", "scss", "toml", "yaml" }, --"maintained", --vim.tbl_keys(require'languages')),
     highlight = {
       enable = true,
     }
