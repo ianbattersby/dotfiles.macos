ef9b1ebf713e58e7022d43aa98ea86b3e988d1c6
Ian Battersby <ian.battersby@gmail.com>
2021-07-07 15:28:55 +0100

    zsh: Add 'gclog' gcloud SDK command for streaming last build.

--- zsh/config/gcloud.config.zsh
+++ zsh/config/gcloud.config.zsh
@@ -1,4 +1,5 @@
 alias gc='gcloud'
+alias gclog="gcloud beta builds log $(gcloud builds list --limit=1 --format='value(id)' --filter='status=(WORKING,QUEUED)') --stream"
 
 export PATH="$PATH:$HOME/.local/google-cloud-sdk/bin"
 export CLOUDSDK_PYTHON=/usr/bin/python3
