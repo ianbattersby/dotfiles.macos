c998d51fba20ec95f576ea212f1fa64fa0599448
Ian Battersby <ian.battersby@gmail.com>
2021-11-03 22:53:12 +0000

    nvim: Fixup nvim-tree configuration.

--- nvim/lua/plugins/navigation/nvim-tree.lua
+++ nvim/lua/plugins/navigation/nvim-tree.lua
@@ -1,23 +1,62 @@
 local function config()
-  local global = {vim.g}
-  require'utils'.opts({
-    nvim_tree_side = 'left',
-    nvim_tree_follow = 1,
-    nvim_tree_quit_on_open = 1,
-    nvim_tree_lsp_diagnostics = 1,
-    nvim_tree_disable_netrw = 0,
-    nvim_tree_hijack_netrw = 0,
-  }, global)
+  vim.cmd[[nnoremap <C-n> :NvimTreeToggle<CR>]]
+  vim.cmd[[nnoremap <leader>r :NvimTreeRefresh<CR>]]
+  vim.cmd[[nnoremap <leader>n :NvimTreeFindFile<CR>]]
 
-  local options = { noremap = true }
-  require'utils'.map('n', '<A-1>', '<CMD>NvimTreeToggle<CR>', options)
+  require'nvim-tree'.setup {
+    disable_netrw       = true,
+    hijack_netrw        = true,
+    open_on_setup       = false,
+    ignore_ft_on_setup  = {},
+    auto_close          = false,
+    open_on_tab         = false,
+    hijack_cursor       = false,
+    update_cwd          = false,
+    update_to_buf_dir   = {
+      enable = true,
+      auto_open = true,
+    },
+    diagnostics = {
+      enable = false,
+      icons = {
+        hint = "",
+        info = "",
+        warning = "",
+        error = "",
+      }
+    },
+    update_focused_file = {
+      enable      = false,
+      update_cwd  = false,
+      ignore_list = {}
+    },
+    system_open = {
+      cmd  = nil,
+      args = {}
+    },
+    filters = {
+      dotfiles = false,
+      custom = {}
+    },
+    view = {
+      width = 30,
+      height = 30,
+      hide_root_folder = false,
+      side = 'left',
+      auto_resize = false,
+      mappings = {
+        custom_only = false,
+        list = {}
+      }
+    }
+  }
 end
 
 return {
   setup = function(use)
     use {
       'kyazdani42/nvim-tree.lua',
-      requires = 'nvim-web-devicons',
+      requires = { 'kyazdani42/nvim-web-devicons' },
       config = config
     }
   end
