735ba7f93909051f8386306a5e384622c67f5a1b
Ian Battersby <ian.battersby@gmail.com>
2021-11-17 15:17:55 +0000

    zsh: Fix Homebrew now that /opt/Homebrew is /opt/homebrew.

--- zsh/config/homebrew.config.zsh
+++ zsh/config/homebrew.config.zsh
@@ -4,12 +4,12 @@ function brew_detect() {
     if [[ "$(uname -m)" == "x86_64" ]]; then
         command arch -x86_64 /usr/local/Homebrew/bin/brew "$@"
     else
-        command arch -arm64 /opt/Homebrew/bin/brew "$@"
+        command arch -arm64 /opt/homebrew/bin/brew "$@"
     fi
 }
 
 alias ibrew='arch -x86_64 /usr/local/Homebrew/bin/brew'
-alias abrew='arch -arm64 /opt/Homebrew/bin/brew'
+alias abrew='arch -arm64 /opt/homebrew/bin/brew'
 alias brew='brew_detect'
 
-export PATH="/opt/Homebrew/bin:$PATH"
+export PATH="/opt/homebrew/bin:$PATH"
