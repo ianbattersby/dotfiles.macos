b11ddbfeb35ef797d4f909f500ff86bc8faaac4c
Ian Battersby <ian.battersby@gmail.com>
2021-11-25 20:42:11 +0000

    nvim: Add 'numb.nvim' for peeking buffer lines.

--- nvim/lua/plugins/navigation/init.lua
+++ nvim/lua/plugins/navigation/init.lua
@@ -4,5 +4,6 @@ return {
     require'plugins.navigation.nvim-tree'.setup(use)
     require'plugins.navigation.tmux-navigator'.setup(use)
     require'plugins.navigation.vim-sneak'.setup(use)
+    require'plugins.navigation.numb'.setup(use)
   end
 }
---
+++ nvim/lua/plugins/navigation/numb.lua
@@ -0,0 +1,12 @@
+local function config()
+  require('numb').setup()
+end
+
+return {
+  setup = function(use)
+    use {
+      'nacro90/numb.nvim',
+      config = config
+    }
+  end
+}
