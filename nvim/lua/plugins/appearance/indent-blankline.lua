390bc4d3b0fb69f256ac5e50a15145c51511be77
Ian Battersby <ian.battersby@gmail.com>
2021-11-25 21:04:28 +0000

    nvim: Add 'indent-blankline' for outlining.

---
+++ nvim/lua/plugins/appearance/indent-blankline.lua
@@ -0,0 +1,23 @@
+local function config()
+  vim.opt.list = true
+  vim.opt.listchars:append("space:⋅")
+  vim.opt.listchars:append("eol:↴")
+
+  require("indent_blankline").setup {
+    buftype_exclude = {"terminal", "telescope"},
+    filetype_exclude = {"help", "dashboard", "packer", "NvimTree", "Trouble"},
+    space_char_blankline = " ",
+    show_current_context = true,
+    show_current_context_start = false,
+    use_treesitter = true,
+  }
+end
+
+return {
+  setup = function(use)
+    use {
+      'lukas-reineke/indent-blankline.nvim',
+      config = config
+    }
+  end
+}
--- nvim/lua/plugins/appearance/init.lua
+++ nvim/lua/plugins/appearance/init.lua
@@ -6,5 +6,6 @@ return {
       require'plugins.appearance.bufferline'.setup(use)
       require'plugins.appearance.startify'.setup(use)
       require'plugins.appearance.floaterm'.setup(use)
+      require'plugins.appearance.indent-blankline'.setup(use)
   end
 }
