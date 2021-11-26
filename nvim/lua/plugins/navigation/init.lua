718c12c44cfb3c993a3f7ef64a845e6335dbb425
Ian Battersby <ian.battersby@gmail.com>
2021-11-26 21:29:59 +0000

    nvim: Rename 'search.lua' to 'telescope.lua'.

--- nvim/lua/plugins/navigation/init.lua
+++ nvim/lua/plugins/navigation/init.lua
@@ -1,6 +1,6 @@
 return {
   setup = function(use)
-    require'plugins.navigation.search'.setup(use)
+    require'plugins.navigation.telescope'.setup(use)
     require'plugins.navigation.nvim-tree'.setup(use)
     require'plugins.navigation.tmux-navigator'.setup(use)
     require'plugins.navigation.lightspeed'.setup(use)
