4d2187610332b51f5a7e4d85ecff2987fb69a482
Ian Battersby <ian.battersby@gmail.com>
2021-11-27 17:42:20 +0000

    nvim: Ops. Formatter isn't working yet.

--- nvim/lua/plugins/editing/init.lua
+++ nvim/lua/plugins/editing/init.lua
@@ -4,6 +4,5 @@ return {
     require'plugins.editing.highlight'.setup(use)
     require'plugins.editing.nvim-autopairs'.setup(use)
     require'plugins.editing.textobj'.setup(use)
-    require'plugins.editing.formatter'.setup(use)
   end
 }
