9d8944683316c2a3a22d0194442fbdac3102e065
Ian Battersby <ian.battersby@gmail.com>
2021-11-27 14:09:06 +0000

    nvim: Improve LSP menu with source indicators/icons.

--- nvim/lua/plugins/lsp/nvim-cmp.lua
+++ nvim/lua/plugins/lsp/nvim-cmp.lua
@@ -1,7 +1,53 @@
 local function config()
   local cmp = require'cmp'
 
+  local kind_icons = {
+    Text = "",
+    Method = "",
+    Function = "",
+    Constructor = "",
+    Field = "",
+    Variable = "",
+    Class = "ﴯ",
+    Interface = "",
+    Module = "",
+    Property = "ﰠ",
+    Unit = "",
+    Value = "",
+    Enum = "",
+    Keyword = "",
+    Snippet = "",
+    Color = "",
+    File = "",
+    Reference = "",
+    Folder = "",
+    EnumMember = "",
+    Constant = "",
+    Struct = "",
+    Event = "",
+    Operator = "",
+    TypeParameter = ""
+  }
+
   cmp.setup({
+    formatting = {
+      format = function(entry, vim_item)
+        -- Kind icons
+        vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+        -- Source
+        vim_item.menu = ({
+          buffer = "[Buffer]",
+          nvim_lsp = "[LSP]",
+          luasnip = "[LuaSnip]",
+          nvim_lua = "[Lua]",
+          latex_symbols = "[LaTeX]",
+        })[entry.source.name]
+        return vim_item
+      end
+    },
+    completion = {
+      completeopt = 'menu,menuone,noinsert',
+    },
     snippet = {
       expand = function(args)
         vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
@@ -42,10 +88,8 @@ local function config()
     })
   })
 
-  vim.cmd("set completeopt=menu,menuone,noselect")
-  vim.cmd("set shortmess+=c")
+  vim.cmd[[set completeopt=menu,menuone,noselect]]
 end
-
 
 return {
   setup = function(use)
