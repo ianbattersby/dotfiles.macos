017ee62af72a45f95d5b6d29a46dbb6d5da05cf1
Ian Battersby <ian.battersby@gmail.com>
2021-12-02 21:48:30 +0000

    nvim: Fix 'nvim-cmp' super-tab completion (finally).

--- nvim/lua/plugins/lsp/_nvim-cmp.lua
+++ nvim/lua/plugins/lsp/_nvim-cmp.lua
@@ -1,8 +1,3 @@
-local has_words_before = function()
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 local function config()
 	local cmp = require("cmp")
 	local cmp_autopairs = require("nvim-autopairs.completion.cmp")
@@ -74,15 +69,24 @@ local function config()
 				c = cmp.mapping.close(),
 			}),
 			["<CR>"] = cmp.mapping.confirm({ select = true }),
+
 			["<Tab>"] = cmp.mapping(function(fallback)
 				if cmp.visible() then
 					cmp.select_next_item()
 				elseif require("luasnip").expand_or_jumpable() then
 					require("luasnip").expand_or_jump()
-				elseif has_words_before() then
-					cmp.complete()
 				else
-					fallback()
+					local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+
+					if
+						col ~= 0
+						and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+					then
+						cmp.complete()
+					else
+						--vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, true, true), "n")
+						fallback()
+					end
 				end
 			end, { "i", "s" }),
 
