0582a943c7d8bafd0f8d71193591aa4e638c926f
Ian Battersby <ian.battersby@gmail.com>
2021-12-04 15:05:41 +0000

    nvim: Switch vim-test strategy 'vimux' -> 'neovim'.

--- nvim/lua/plugins/lsp/_vim-ultest.lua
+++ nvim/lua/plugins/lsp/_vim-ultest.lua
@@ -1,6 +1,6 @@
 local function config()
 	vim.g.ultest_use_pty = 1
-	vim.g["test#strategy"] = { nearest = "vimux", file = "vimux", suite = "vimux" }
+	vim.g["test#strategy"] = { nearest = "neovim", file = "neovim", suite = "neovim" }
 	vim.g["test#enabled_runner"] = { "python#pytest", "go#gotest", "lua#busted", "rust#cargotest" }
 	vim.g["VimuxHeight"] = 40
 	vim.g["VimuxUseNearest"] = 1
@@ -13,7 +13,7 @@ return {
 	setup = function(use)
 		use({
 			"rcarriga/vim-ultest",
-			requires = { { "vim-test/vim-test" }, { "preservim/vimux" } },
+			requires = { "vim-test/vim-test" },
 			run = ":UpdateRemotePlugins",
 			config = config,
 		})
