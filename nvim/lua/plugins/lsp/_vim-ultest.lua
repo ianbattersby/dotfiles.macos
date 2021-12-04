49d271ab62f294096247dc4e62a8141939344c57
Ian Battersby <ian.battersby@gmail.com>
2021-12-04 21:14:07 +0000

    nvim: Tweak 'nvim-test' settings (still not happy).

--- nvim/lua/plugins/lsp/_vim-ultest.lua
+++ nvim/lua/plugins/lsp/_vim-ultest.lua
@@ -1,12 +1,20 @@
 local function config()
 	vim.g.ultest_use_pty = 1
+
 	vim.g["test#strategy"] = { nearest = "neovim", file = "neovim", suite = "neovim" }
-	vim.g["test#enabled_runner"] = { "python#pytest", "go#gotest", "lua#busted", "rust#cargotest" }
-	vim.g["VimuxHeight"] = 40
-	vim.g["VimuxUseNearest"] = 1
-	vim.g["VimuxOrientation"] = "h"
-	vim.g["VimuxRunnerName"] = "vm-test"
-	vim.g["VimuxCloseOnExit"] = 1
+
+	vim.g["test#enabled_runners"] = {
+		"javascript#ava",
+		"javascript#cucumberjs",
+		"python#pytest",
+		"python#behave",
+		"go#gotest",
+		"lua#busted",
+		"rust#cargotest",
+	}
+
+	--These don't work due to some "Dictionary as String" error :'(
+	--vim.g["test#runner_commands"] = { "Ava", "CucumberJS", "Busted", "CargoTest" }
 end
 
 return {
