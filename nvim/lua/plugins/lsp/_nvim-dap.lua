b5cc3a1ceddd485955c1ce328cba69ba94f5dae9
Ian Battersby <ian.battersby@gmail.com>
2021-12-05 15:31:53 +0000

    nvim: Pick better key mappings for 'nvim-dap'.

--- nvim/lua/plugins/lsp/_nvim-dap.lua
+++ nvim/lua/plugins/lsp/_nvim-dap.lua
@@ -4,6 +4,109 @@ local function config()
 	local dapgo = require("dap-go")
 	local dappython = require("dap-python")
 
+	dapui.setup({
+		icons = { expanded = "▾", collapsed = "▸" },
+		mappings = {
+			-- Use a table to apply multiple mappings
+			expand = { "<CR>", "<2-LeftMouse>" },
+			open = "o",
+			remove = "d",
+			edit = "e",
+			repl = "r",
+		},
+		sidebar = {
+			--open_on_start = false,
+			-- You can change the order of elements in the sidebar
+			elements = {
+				-- Provide as ID strings or tables with "id" and "size" keys
+				{
+					id = "scopes",
+					size = 0.25, -- Can be float or integer > 1
+				},
+				{ id = "breakpoints", size = 0.25 },
+				{ id = "stacks", size = 0.25 },
+				{ id = "watches", size = 00.25 },
+			},
+			size = 40,
+			position = "left", -- Can be "left", "right", "top", "bottom"
+		},
+		tray = {
+			--open_on_start = true,
+			elements = { "repl" },
+			size = 10,
+			position = "bottom", -- Can be "left", "right", "top", "bottom"
+		},
+		floating = {
+			max_height = nil, -- These can be integers or a float between 0 and 1.
+			max_width = nil, -- Floats will be treated as percentage of your screen.
+			mappings = {
+				close = { "q", "<Esc>" },
+			},
+		},
+		windows = { indent = 1 },
+	})
+
+	-- Automatically open UI
+	dap.listeners.after.event_initialized["dapui_config"] = function()
+		dapui.open()
+	end
+	dap.listeners.before.event_terminated["dapui_config"] = function()
+		dapui.close()
+	end
+	dap.listeners.before.event_exited["dapui_config"] = function()
+		dapui.close()
+	end
+
+	-- Key mapping
+	vim.api.nvim_set_keymap(
+		"n",
+		"<Leader>db",
+		"<CMD>lua require('dap').toggle_breakpoint()<CR>",
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_set_keymap(
+		"n",
+		"<Leader>dc",
+		"<CMD>lua require('dap').continue()<CR>",
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_set_keymap(
+		"n",
+		"<Leader>dd",
+		"<CMD>lua require('dap').continue()<CR>",
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_set_keymap(
+		"n",
+		"<Leader>dh",
+		"<CMD>lua require('dap.ui.variables').hover()<CR>",
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_set_keymap(
+		"n",
+		"<Leader>di",
+		"<CMD>lua require('dap').step_into()<CR>",
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_set_keymap(
+		"n",
+		"<Leader>do",
+		"<CMD>lua require('dap').step_out()<CR>",
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_set_keymap(
+		"n",
+		"<Leader>dO",
+		"<CMD>lua require('dap').step_over()<CR>",
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_set_keymap(
+		"n",
+		"<Leader>ds",
+		"<CMD>lua require('dap.ui.variables').scopes()<CR>",
+		{ noremap = true, silent = true }
+	)
+
 	-- Configure different adapters
 	dap.adapters.nlua = function(callback, dapconfig)
 		callback({ type = "server", host = dapconfig.host, port = dapconfig.port })
@@ -85,91 +188,13 @@ local function config()
 	dappython.setup("~/.virtualenvs/debugpy/bin/python")
 	dappython.test_runner = "pytest"
 
-	vim.api.nvim_set_keymap(
-		"n",
-		"<leader>b",
-		"<CMD>lua require'dap'.toggle_breakpoint()<CR>",
-		{ silent = true, noremap = true }
-	)
-	vim.api.nvim_set_keymap(
-		"n",
-		"<leader>B",
-		"<CMD>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
-		{ silent = true, noremap = true }
-	)
-	vim.api.nvim_set_keymap(
-		"n",
-		"<leader>lp",
-		"<CMD>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>",
-		{ silent = true, noremap = true }
-	)
-	vim.api.nvim_set_keymap(
-		"n",
-		"<leader>dr",
-		"<CMD>lua require'dap'.repl.open()<CR>",
-		{ silent = true, noremap = true }
-	)
-	vim.api.nvim_set_keymap(
-		"n",
-		"<leader>dl",
-		"<CMD>lua require'dap'.run_last()<CR>",
-		{ silent = true, noremap = true }
-	)
-	vim.api.nvim_set_keymap(
-		"n",
-		"<leader>y",
-		"<CMD>lua require'dap-go'.debug_test()<CR>",
-		{ silent = true, noremap = true }
-	)
-
 	-- Javascript configuration is waiting on support for 'vscode-js-debug'
 	-- in nvim-dap (as an extension) and for Apple arm64. Given I hate JS
 	-- anyway, this shouldn't be any great shakes.
 	--
 	-- see: https://github.com/mfussenegger/nvim-dap/issues/82
 
-	dapui.setup({
-		icons = { expanded = "▾", collapsed = "▸" },
-		mappings = {
-			-- Use a table to apply multiple mappings
-			expand = { "<CR>", "<2-LeftMouse>" },
-			open = "o",
-			remove = "d",
-			edit = "e",
-			repl = "r",
-		},
-		sidebar = {
-			--open_on_start = false,
-			-- You can change the order of elements in the sidebar
-			elements = {
-				-- Provide as ID strings or tables with "id" and "size" keys
-				{
-					id = "scopes",
-					size = 0.25, -- Can be float or integer > 1
-				},
-				{ id = "breakpoints", size = 0.25 },
-				{ id = "stacks", size = 0.25 },
-				{ id = "watches", size = 00.25 },
-			},
-			size = 40,
-			position = "left", -- Can be "left", "right", "top", "bottom"
-		},
-		tray = {
-			--open_on_start = true,
-			elements = { "repl" },
-			size = 10,
-			position = "bottom", -- Can be "left", "right", "top", "bottom"
-		},
-		floating = {
-			max_height = nil, -- These can be integers or a float between 0 and 1.
-			max_width = nil, -- Floats will be treated as percentage of your screen.
-			mappings = {
-				close = { "q", "<Esc>" },
-			},
-		},
-		windows = { indent = 1 },
-	})
-
+	-- Enable virtual text
 	require("nvim-dap-virtual-text").setup()
 end
 
