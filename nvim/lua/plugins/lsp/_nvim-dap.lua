38bab53a1547f8bedc5e984a831240a5e172e801
Ian Battersby <ian.battersby@gmail.com>
2021-12-05 11:33:57 +0000

    nvim: Extend 'nvim-dap' adapters and configurations (WIP).

--- nvim/lua/plugins/lsp/_nvim-dap.lua
+++ nvim/lua/plugins/lsp/_nvim-dap.lua
@@ -2,13 +2,52 @@ local function config()
 	local dap = require("dap")
 	local dapui = require("dapui")
 	local dapgo = require("dap-go")
+	local dappython = require("dap-python")
 
+	-- Configure different adapters
+	dap.adapters.nlua = function(callback, dapconfig)
+		callback({ type = "server", host = dapconfig.host, port = dapconfig.port })
+	end
+
+	-- Downloaded from https://github.com/vadimcn/vscode-lldb/releases
 	dap.adapters.lldb = {
 		type = "executable",
-		command = "/usr/bin/lldb-vscode", -- adjust as needed
+		command = "/Users/ian/.local/codelldb-aarch64-darwin/extension/adapter/codelldb",
 		name = "lldb",
 	}
 
+	-- Currently experimental, sourced manually from MS repository:
+	-- https://github.com/microsoft/vscode-cpptools/releases/tag/1.7.1
+	--
+	-- Follow: https://github.com/Pocco81/DAPInstall.nvim/blob/master/lua/dap-install/core/debuggers/ccppr_vsc.lua#L49-L57
+	-- dap.adapters.cpptools = {
+	-- 	type = "executable",
+	-- 	command = "/Users/ian/.local/cpptools-osx-arm64/extension/debugAdapters/bin/OpenDebugAD7",
+	-- 	name = "cpptools",
+	-- }
+
+	-- Lua configuration using 'one-small-step-for-vimkind'
+	dap.configurations.lua = {
+		{
+			type = "nlua",
+			request = "attach",
+			name = "Attach to running Neovim instance",
+			host = function()
+				local value = vim.fn.input("Host [127.0.0.1]: ")
+				if value ~= "" then
+					return value
+				end
+				return "127.0.0.1"
+			end,
+			port = function()
+				local val = tonumber(vim.fn.input("Port: "))
+				assert(val, "Please provide a port number")
+				return val
+			end,
+		},
+	}
+
+	-- Configure CPP to use lldb
 	dap.configurations.cpp = {
 		{
 			name = "Launch",
@@ -35,8 +74,16 @@ local function config()
 		},
 	}
 
+	-- C and Rust can use CPP configuration
 	dap.configurations.c = dap.configurations.cpp
 	dap.configurations.rust = dap.configurations.cpp
+
+	-- Go configuration using 'dap-go' extension
+	dapgo.setup()
+
+	-- Python configuration using 'dap-python' extension
+	dappython.setup("~/.virtualenvs/debugpy/bin/python")
+	dappython.test_runner = "pytest"
 
 	vim.api.nvim_set_keymap(
 		"n",
@@ -68,15 +115,18 @@ local function config()
 		"<CMD>lua require'dap'.run_last()<CR>",
 		{ silent = true, noremap = true }
 	)
-
-	dapgo.setup()
-
 	vim.api.nvim_set_keymap(
 		"n",
 		"<leader>y",
 		"<CMD>lua require'dap-go'.debug_test()<CR>",
 		{ silent = true, noremap = true }
 	)
+
+	-- Javascript configuration is waiting on support for 'vscode-js-debug'
+	-- in nvim-dap (as an extension) and for Apple arm64. Given I hate JS
+	-- anyway, this shouldn't be any great shakes.
+	--
+	-- see: https://github.com/mfussenegger/nvim-dap/issues/82
 
 	dapui.setup({
 		icons = { expanded = "▾", collapsed = "▸" },
@@ -125,7 +175,12 @@ return {
 	setup = function(use)
 		use({
 			"mfussenegger/nvim-dap",
-			requires = { { "rcarriga/nvim-dap-ui" }, { "leoluz/nvim-dap-go" } },
+			requires = {
+				{ "rcarriga/nvim-dap-ui" },
+				{ "leoluz/nvim-dap-go" },
+				{ "mfussenegger/nvim-dap-python" },
+				{ "jbyuki/one-small-step-for-vimkind" },
+			},
 			config = config,
 		})
 	end,
