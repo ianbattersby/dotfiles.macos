f32729409086d76e09792d48957c7eb08b28a21b
Ian Battersby <ian.battersby@gmail.com>
2021-12-05 15:24:17 +0000

    nvim: Experiment w/0.6.0 built-in actions (vs lsputils).

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -3,7 +3,7 @@ return {
 		require("plugins.lsp._null-ls").setup(use)
 		require("plugins.lsp._nvim-cmp").setup(use)
 		require("plugins.lsp._lspconfig").setup(use)
-		require("plugins.lsp._lsputils").setup(use)
+		--require("plugins.lsp._lsputils").setup(use)
 		require("plugins.lsp._trouble").setup(use)
 		require("plugins.lsp._treesitter").setup(use)
 		require("plugins.lsp._vim-ultest").setup(use)
