dbe96d6df615d6c4e9ddd29b503078634bc730ab
Ian Battersby <ian.battersby@gmail.com>
2021-12-06 22:40:31 +0000

    nvim: Remove 'lightspeed.nvim' deprecated opt.

--- nvim/lua/plugins/navigation/_lightspeed.lua
+++ nvim/lua/plugins/navigation/_lightspeed.lua
@@ -6,7 +6,7 @@ local function config()
 		grey_out_search_area = true,
 		match_only_the_start_of_same_char_seqs = true,
 		limit_ft_matches = 5,
-		full_inclusive_prefix_key = "<c-m>",
+		--full_inclusive_prefix_key = "<c-m>",
 	})
 end
 
