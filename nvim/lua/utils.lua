92aac46be8d0a867a51b33b4527f80bf5513b675
Ian Battersby <ian.battersby@gmail.com>
2021-12-07 19:28:33 +0000

    nvim: Add 'tprint' table debug function to 'utils'.

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -33,4 +33,31 @@ local function find_session_directory()
 	return vim.fn.getcwd()
 end
 
-return { opts = set_opts, opt = opt, map = map, find_session_directory = find_session_directory }
+	local function tprint(tbl, indent)
+		if not indent then
+			indent = 0
+		end
+		local toprint = string.rep(" ", indent) .. "{\n"
+		indent = indent + 2
+		for k, v in pairs(tbl) do
+			toprint = toprint .. string.rep(" ", indent)
+			if type(k) == "number" then
+				toprint = toprint .. "[" .. k .. "] = "
+			elseif type(k) == "string" then
+				toprint = toprint .. k .. "= "
+			end
+			if type(v) == "number" then
+				toprint = toprint .. v .. ",\n"
+			elseif type(v) == "string" then
+				toprint = toprint .. '"' .. v .. '",\n'
+			elseif type(v) == "table" then
+				toprint = toprint .. tprint(v, indent + 2) .. ",\n"
+			else
+				toprint = toprint .. '"' .. tostring(v) .. '",\n'
+			end
+		end
+		toprint = toprint .. string.rep(" ", indent - 2) .. "}"
+		return toprint
+	end
+
+return { opts = set_opts, opt = opt, map = map, find_session_directory = find_session_directory, tprint = tprint}
