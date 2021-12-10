faf06ff3fe145af692c70712b9364e58689739e8
Ian Battersby <ian.battersby@gmail.com>
2021-12-10 13:38:19 +0000

    nvim: 'lightspeed.nvim' has deprecated 'jump_to_first_match'.

--- nvim/lua/plugins/navigation/_lightspeed.lua
+++ nvim/lua/plugins/navigation/_lightspeed.lua
@@ -1,6 +1,6 @@
 local function config()
 	require("lightspeed").setup({
-		jump_to_first_match = true,
+		--jump_to_first_match = true,
 		jump_on_partial_input_safety_timeout = 400,
 		highlight_unique_chars = true,
 		grey_out_search_area = true,
