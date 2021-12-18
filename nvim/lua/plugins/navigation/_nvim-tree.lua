048775231c1679bcab463ecae25a94ad1b11290e
Ian Battersby <ian.battersby@gmail.com>
2021-12-18 14:46:07 +0000

    nvim: Another attempt to resolve compile loading order.

--- nvim/lua/plugins/navigation/_nvim-tree.lua
+++ nvim/lua/plugins/navigation/_nvim-tree.lua
@@ -1,63 +1,64 @@
 local function config()
-	vim.cmd([[nnoremap <C-n> :NvimTreeToggle<CR>]])
-	vim.cmd([[nnoremap <leader>r :NvimTreeRefresh<CR>]])
-	vim.cmd([[nnoremap <leader>n :NvimTreeFindFile<CR>]])
+  vim.cmd([[nnoremap <C-n> :NvimTreeToggle<CR>]])
+  vim.cmd([[nnoremap <leader>r :NvimTreeRefresh<CR>]])
+  vim.cmd([[nnoremap <leader>n :NvimTreeFindFile<CR>]])
 
-	require("nvim-tree").setup({
-		disable_netrw = true,
-		hijack_netrw = true,
-		open_on_setup = false,
-		ignore_ft_on_setup = {},
-		auto_close = true,
-		open_on_tab = false,
-		hijack_cursor = false,
-		update_cwd = false,
-		update_to_buf_dir = {
-			enable = true,
-			auto_open = true,
-		},
-		update_focused_file = {
-			enable = true,
-			update_cwd = true,
-			ignore_list = {},
-		},
-		diagnostics = {
-			enable = false,
-			icons = {
-				hint = "",
-				info = "",
-				warning = "",
-				error = "",
-			},
-		},
-		system_open = {
-			cmd = nil,
-			args = {},
-		},
-		filters = {
-			dotfiles = false,
-			custom = {},
-		},
-		view = {
-			width = 35,
-			height = 30,
-			hide_root_folder = false,
-			side = "left",
-			auto_resize = false,
-			mappings = {
-				custom_only = false,
-				list = {},
-			},
-		},
-	})
+  require("nvim-tree").setup({
+    disable_netrw = true,
+    hijack_netrw = true,
+    open_on_setup = false,
+    ignore_ft_on_setup = {},
+    auto_close = true,
+    open_on_tab = false,
+    hijack_cursor = false,
+    update_cwd = false,
+    update_to_buf_dir = {
+      enable = true,
+      auto_open = true,
+    },
+    update_focused_file = {
+      enable = true,
+      update_cwd = true,
+      ignore_list = {},
+    },
+    diagnostics = {
+      enable = false,
+      icons = {
+        hint = "",
+        info = "",
+        warning = "",
+        error = "",
+      },
+    },
+    system_open = {
+      cmd = nil,
+      args = {},
+    },
+    filters = {
+      dotfiles = false,
+      custom = {},
+    },
+    view = {
+      width = 35,
+      height = 30,
+      hide_root_folder = false,
+      side = "left",
+      auto_resize = false,
+      mappings = {
+        custom_only = false,
+        list = {},
+      },
+    },
+  })
 end
 
 return {
-	setup = function(use)
-		use({
-			"kyazdani42/nvim-tree.lua",
-			requires = { "kyazdani42/nvim-web-devicons" },
-			config = config,
-		})
-	end,
+  setup = function(use)
+    use({
+      "kyazdani42/nvim-tree.lua",
+      requires = { "kyazdani42/nvim-web-devicons" },
+      after = { "onedark.nvim", "nvim-web-devicons" },
+      config = config,
+    })
+  end,
 }
