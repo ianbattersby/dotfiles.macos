6ee6af0f2f0484b517c904f26a4ba8b779a7e63d
Ian Battersby <ian.battersby@gmail.com>
2021-12-23 16:16:16 +0000

    nvim: Consolidate use of 'trouble.nvim' for LSP functions.

--- nvim/lua/plugins/lsp/_trouble.lua
+++ nvim/lua/plugins/lsp/_trouble.lua
@@ -1,22 +1,25 @@
 local function config()
-  require("trouble").setup {}
+  require("trouble").setup {
+    position = "bottom",
+    height = 10,
+    auto_open = true,
+    auto_close = false,
+  }
 
   vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { silent = true, noremap = true })
   vim.api.nvim_set_keymap(
     "n",
     "<leader>xw",
-    "<cmd>Trouble lsp_workspace_diagnostics<cr>",
+    "<cmd>TroubleToggle workspace_diagnostics<cr>",
     { silent = true, noremap = true }
   )
-  vim.api.nvim_set_keymap(
-    "n",
-    "<leader>xd",
-    "<cmd>Trouble lsp_document_diagnostics<cr>",
-    { silent = true, noremap = true }
-  )
-  vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>", { silent = true, noremap = true })
+  -- vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>", {
+  --   silent = true,
+  --   noremap = true,
+  -- })
+  --vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>", { silent = true, noremap = true })
   vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>", { silent = true, noremap = true })
-  vim.api.nvim_set_keymap("n", "gR", "<cmd>Trouble lsp_references<cr>", { silent = true, noremap = true })
+  --vim.api.nvim_set_keymap("n", "gR", "<cmd>Trouble lsp_references<cr>", { silent = true, noremap = true })
 end
 
 return {
