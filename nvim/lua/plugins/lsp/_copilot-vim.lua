c05becf5ba48056ab3813b656f0e34b5618d4393
Ian Battersby <ian.battersby@gmail.com>
2021-12-23 23:17:53 +0000

    nvim: Fix 'copilot-vim' keymapping conflict.

--- nvim/lua/plugins/lsp/_copilot-vim.lua
+++ nvim/lua/plugins/lsp/_copilot-vim.lua
@@ -1,4 +1,7 @@
-local function config() end
+local function config()
+  vim.g.copilot_no_tab_map = true
+  vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+end
 
 return {
   setup = function(use)
