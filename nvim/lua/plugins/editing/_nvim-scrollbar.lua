2fbf620c89c7e7809217af1bc72866df6f4bb094
Ian Battersby <ian.battersby@gmail.com>
2022-01-07 22:05:08 +0000

    nvim: Tweak 'nvim-scrollbar' for colors.

--- nvim/lua/plugins/editing/_nvim-scrollbar.lua
+++ nvim/lua/plugins/editing/_nvim-scrollbar.lua
@@ -1,11 +1,27 @@
 local function config()
-  require("scrollbar").setup()
+  local theme = require("plugins.appearance._theme").name
+  local colors = require(theme .. ".colors").setup()
+
+  require("scrollbar").setup {
+    handle = {
+      color = colors.bg_highlight,
+    },
+    marks = {
+      Search = { color = colors.orange },
+      Error = { color = colors.error },
+      Warn = { color = colors.warning },
+      Info = { color = colors.info },
+      Hint = { color = colors.hint },
+      Misc = { color = colors.purple },
+    },
+  }
 end
 
 return {
   setup = function(use)
     use {
       "petertriho/nvim-scrollbar",
+      after = "onedark.nvim",
       config = config,
     }
   end,
