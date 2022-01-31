26d9d2012ff5a102cdb0d99600038e9aae298204
Ian Battersby <ian.battersby@gmail.com>
2022-01-31 21:28:41 +0000

    nvim: Change 'lsp_signature' border to be consistent.

--- nvim/lua/lspbuilder.lua
+++ nvim/lua/lspbuilder.lua
@@ -47,7 +47,7 @@ function M:on_attach()
       max_height = 22,
       max_width = 120, -- max_width of signature floating_window, line will be wrapped if exceed max_width
       handler_opts = {
-        border = "single", -- double, single, shadow, none
+        border = "rounded", -- double, rounded, single, shadow, none
       },
       zindex = 200, -- by default it will be on top of all floating windows, set to 50 send it to bottom
       padding = "", -- character to pad on left and right of signature can be ' ', or '|'  etc
