e4911412e097cb19857a455129407f02cea8dfa4
Ian Battersby <ian.battersby@gmail.com>
2022-02-04 23:09:36 +0000

    nvim: Enable extension 'comment.nvim' key-mappings.

--- nvim/lua/plugins/editing/_comment.lua
+++ nvim/lua/plugins/editing/_comment.lua
@@ -1,11 +1,19 @@
 local function config()
-  require("Comment").setup()
+  require("Comment").setup {
+    ignore = "^$",
+    mappings = {
+      basic = true,
+      extra = true,
+      extended = true,
+    },
+  }
 end
 
 return {
   setup = function(use)
     use {
       "numToStr/Comment.nvim",
+      after = "nvim-treesitter",
       config = config,
     }
   end,
