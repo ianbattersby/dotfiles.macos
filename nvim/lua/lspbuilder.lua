3048ed20a93cfa5efce3814de8e78f93cfcc793d
Ian Battersby <ian.battersby@gmail.com>
2022-03-07 20:30:10 +0000

    nvim: Move 'ray-x/lsp_signature' config (better perf?).

--- nvim/lua/lspbuilder.lua
+++ nvim/lua/lspbuilder.lua
@@ -136,25 +136,6 @@ function M:on_attach()
 
     -- Register lsp-status for updates
     require("lsp-status").on_attach(client)
-
-    -- Register lsp_signature for signature help
-    require("lsp_signature").on_attach({
-      bind = true,
-      doc_lines = 0,
-      floating_window = true,
-      fix_pos = true,
-      hint_enable = true,
-      hint_prefix = " ",
-      hint_scheme = "String",
-      hi_parameter = "Search",
-      max_height = 22,
-      max_width = 120, -- max_width of signature floating_window, line will be wrapped if exceed max_width
-      handler_opts = {
-        border = "rounded", -- double, rounded, single, shadow, none
-      },
-      zindex = 200, -- by default it will be on top of all floating windows, set to 50 send it to bottom
-      padding = "", -- character to pad on left and right of signature can be ' ', or '|'  etc
-    }, bufnr)
   end
 end
 
