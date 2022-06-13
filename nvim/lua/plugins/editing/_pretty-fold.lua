f17cd34b98af7c3d429218e6f0b3045349a3d63d
Ian Battersby <ian.battersby@gmail.com>
2022-06-13 21:29:17 +0100

    nvim: Fix required dependency for 'pretty-fold.nvim'.

--- nvim/lua/plugins/editing/_pretty-fold.lua
+++ nvim/lua/plugins/editing/_pretty-fold.lua
@@ -1,12 +1,13 @@
 local function config()
   require("pretty-fold").setup {}
-  require("pretty-fold.preview").setup_keybinding "l"
+  require("pretty-fold.preview").setup {}
 end
 
 return {
   setup = function(use)
     use {
       "anuvyklack/pretty-fold.nvim",
+      requires = 'anuvyklack/nvim-keymap-amend',
       config = config,
     }
   end,
