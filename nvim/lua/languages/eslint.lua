00435191cceca3c2ba6da1a5d86b7d65b178b853
Ian Battersby <ian.battersby@gmail.com>
2022-09-15 11:07:51 +0100

    nvim: Fix 'eslint' lsp server configuration.

--- nvim/lua/languages/eslint.lua
+++ nvim/lua/languages/eslint.lua
@@ -1,5 +1,19 @@
+local config = {
+  enable = true,
+  format = { enable = true }, -- this will enable formatting
+  packageManager = "npm",
+  autoFixOnSave = true,
+  codeActionsOnSave = {
+    mode = "all",
+    rules = { "!debugger", "!no-only-tests/*" },
+  },
+  lintTask = {
+    enable = true,
+  },
+}
+
 local function finalize()
-  vim.cmd [[autocmd BufWritePre *.tsx,*.ts,*.jsx,*.js EslintFixAll]]
+  --vim.cmd [[autocmd BufWritePre *.tsx,*.ts,*.jsx,*.js EslintFixAll]]
 end
 
-return { server = "eslint", config = {}, finalize = finalize }
+return { server = "eslint", config = config, finalize = finalize }
