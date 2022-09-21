795343aec0357440404e78389a212ca5c988f94d
Ian Battersby <ian.battersby@gmail.com>
2022-09-21 11:15:16 +0100

    Remove supplemental filetypes and experimental native.

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -22,24 +22,11 @@ local function packer_setup()
       use "wbthomason/packer.nvim"
       use "lewis6991/impatient.nvim"
 
-      -- use {
-      --   "nathom/filetype.nvim",
-      --   config = function()
-      --     require("filetype").setup {
-      --       overrides = {
-      --         extensions = {
-      --           tf = "terraform",
-      --         },
-      --       },
-      --     }
-      --   end,
-      -- }
-
       -- Use experimental built-in filetype lua-based support
       vim.g.do_filetype_lua = 1
 
       -- Do not source the default filetype.vim
-      --vim.g.did_load_filetypes = 0
+      vim.g.did_load_filetypes = 0
 
       -- Ensure we source packer_compiled from custom path
       --require("packer_compiled")
