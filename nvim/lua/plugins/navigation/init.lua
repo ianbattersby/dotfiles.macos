289b2ba15cd429f541552a6da9ba65bb87d51ac9
Ian Battersby <ian.battersby@gmail.com>
2022-09-21 10:11:26 +0100

    nvim: Remove 'lightspeed.nvim' and defer to native.

--- nvim/lua/plugins/navigation/init.lua
+++ nvim/lua/plugins/navigation/init.lua
@@ -1,6 +1,5 @@
 return {
   setup = function(use)
-    require("plugins.navigation._lightspeed").setup(use)
     require("plugins.navigation._numb").setup(use)
     require("plugins.navigation._nvim-tree").setup(use)
     require("plugins.navigation._telescope").setup(use)
