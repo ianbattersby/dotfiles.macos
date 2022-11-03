13759ec403c88be2dbbf7bafd2eb439e7547acb6
Ian Battersby <ian.battersby@gmail.com>
2022-11-03 20:19:30 +0000

    nvim: Remove erroneous 'null-ls' sources.

--- nvim/lua/plugins/lsp/_null-ls.lua
+++ nvim/lua/plugins/lsp/_null-ls.lua
@@ -4,9 +4,7 @@ local function config()
   null_ls.setup {
     sources = {
       null_ls.builtins.formatting.stylua,
-      --null_ls.builtins.formatting.terraform_fmt,
       null_ls.builtins.code_actions.shellcheck,
-      null_ls.builtins.diagnostics.pylint,
       null_ls.builtins.diagnostics.markdownlint,
     },
   }
