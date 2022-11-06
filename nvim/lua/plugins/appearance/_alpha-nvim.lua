449f89bdb123e88682f424f02e5e96a2a3c07054
Ian Battersby <ian.battersby@gmail.com>
2022-11-06 22:40:32 +0000

    Add 'Last session' shortcut to 'alpha.nvim' startup.

--- nvim/lua/plugins/appearance/_alpha-nvim.lua
+++ nvim/lua/plugins/appearance/_alpha-nvim.lua
@@ -1,5 +1,14 @@
 local function config()
-  require("alpha").setup(require("alpha.themes.startify").opts)
+  local startify = require "alpha.themes.startify"
+
+  local top_buttons = {
+    startify.button("e", "New file", "<CMD>ene <CR>"),
+    startify.button("/", "Last session", "<CMD>SessionManager load_last_session<CR>"),
+  }
+
+  startify.config.layout[4] = vim.tbl_deep_extend("keep", { val = top_buttons }, startify.config.layout[4])
+
+  require("alpha").setup(startify.config)
 
   vim.keymap.set("n", "<leader>a", "<CMD>Alpha<CR>", { noremap = true, silent = true, desc = "Startup" })
 end
