1f31c4192b5e2404bb1ab0c191290780b22ace8d
Ian Battersby <ian.battersby@gmail.com>
2022-11-08 22:11:48 +0000

    repo: Remove weechat and associated config.

--- zsh/config/weechat.config.zsh
+++
@@ -1,4 +0,0 @@
-alias weechat='LD_PRELOAD=~/.weechat/libwcwidth.so /usr/local/bin/weechat'
-
-#PERL_MM_OPT="INSTALL_BASE=$HOME/perl5" cpan local::lib
-#echo 'eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib=$HOME/perl5)"' 
