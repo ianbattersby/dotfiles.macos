707783d27ff652d0b74f19e6b99b875c80c0ae61
Ian Battersby <ian.battersby@gmail.com>
2022-11-16 13:01:25 +0000

    nvim: Enable 'indent-blankline.nvim' colored indents;
      * In the hopes it solves the rougue colour issue.

--- nvim/lua/plugins/appearance/_indent-blankline.lua
+++ nvim/lua/plugins/appearance/_indent-blankline.lua
@@ -20,7 +20,7 @@ local function config()
     show_current_context = true,
     show_current_context_start = false,
     use_treesitter = true,
-    colored_indent_levels = false,
+    colored_indent_levels = true,
     char_highlight_list = { "VertSplit" },
   }
 end
