7c1cd2b9dab5f894bcd14db4ef98288f58c2929b
Ian Battersby <ian.battersby@gmail.com>
2023-08-15 17:18:53 +0100

    nvim: Add 'wildfire.nvim' for better incremental selection.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -10,8 +10,8 @@ return {
         "nvim-treesitter/nvim-treesitter-textobjects",
         init = function()
           -- PERF: no need to load the plugin, if we only need its queries for mini.ai
-          local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
-          local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+          local plugin = require "lazy.core.config".spec.plugins["nvim-treesitter"]
+          local opts = require "lazy.core.plugin".values(plugin, "opts", false)
           local enabled = false
           if opts.textobjects then
             for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
@@ -22,14 +22,14 @@ return {
             end
           end
           if not enabled then
-            require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+            require "lazy.core.loader".disable_rtp_plugin "nvim-treesitter-textobjects"
           end
         end,
       },
     },
     keys = {
       { "<c-space>", desc = "Increment selection" },
-      { "<bs>", desc = "Decrement selection", mode = "x" },
+      { "<bs>",      desc = "Decrement selection", mode = "x" },
     },
     ---@type TSConfig
     opts = {
@@ -55,15 +55,16 @@ return {
         "vimdoc",
         "yaml",
       },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
+      -- Let wildfire.nvim handle this!
+      -- incremental_selection = {
+      --   enable = true,
+      --   keymaps = {
+      --     init_selection = "<C-space>",
+      --     node_incremental = "<C-space>",
+      --     scope_incremental = false,
+      --     node_decremental = "<bs>",
+      --   },
+      -- },
     },
     ---@param opts TSConfig
     config = function(_, opts)
@@ -78,8 +79,7 @@ return {
           return true
         end, opts.ensure_installed)
       end
-      require("nvim-treesitter.configs").setup(opts)
+      require "nvim-treesitter.configs".setup(opts)
     end,
   },
 }
-
---
+++ nvim/lua/plugins/wildfile-nvim.lua
@@ -0,0 +1,10 @@
+local M = {
+  "sustech-data/wildfire.nvim",
+  event = "VeryLazy",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  config = function()
+    require "wildfire".setup()
+  end,
+}
+
+return M
