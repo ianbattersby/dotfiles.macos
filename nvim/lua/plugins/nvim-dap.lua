0b9531acfbaa5df41ef2e17a68ff7f63b0cf42c7
Ian Battersby <ian.battersby@gmail.com>
2023-09-02 20:29:28 +0100

    nvim: Add 'venv-selector.nvim'.

--- nvim/lua/plugins/nvim-dap.lua
+++ nvim/lua/plugins/nvim-dap.lua
@@ -52,7 +52,18 @@ local M = {
     },
     "theHamsta/nvim-dap-virtual-text",
     "leoluz/nvim-dap-go",
-    "mfussenegger/nvim-dap-python",
+    {
+      "mfussenegger/nvim-dap-python",
+      -- stylua: ignore
+      keys = {
+        { "<leader>dPt", function() require "dap-python".test_method() end, desc = "Debug Method" },
+        { "<leader>dPc", function() require "dap-python".test_class() end, desc = "Debug Class" },
+      },
+      config = function()
+        local path = require "mason-registry".get_package "debugpy":get_install_path()
+        require "dap-python".setup(path .. "/venv/bin/python")
+      end,
+    },
     "jbyuki/one-small-step-for-vimkind",
     "ianbattersby/telescope.nvim",
     "nvim-telescope/telescope-dap.nvim",
