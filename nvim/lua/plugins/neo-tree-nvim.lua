46504c26ebe9f7428bf63b68aafa89b079841691
Ian Battersby <ian.battersby@gmail.com>
2024-01-06 15:18:51 +0000

    nvim: Always show 'filtered_items' in 'neo-tree'.

--- nvim/lua/plugins/neo-tree-nvim.lua
+++ nvim/lua/plugins/neo-tree-nvim.lua
@@ -76,6 +76,12 @@ local M = {
       bind_to_cwd = false,
       follow_current_file = { enabled = true },
       use_libuv_file_watcher = true,
+      filtered_items = {
+        always_show = {
+          ".gitignore",
+          ".azure_pipelines",
+        },
+      },
     },
     window = {
       mappings = {
